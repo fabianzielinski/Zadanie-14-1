f87f27074db72d59a022d7bc7255055e3fb8ede2
fabianzielinski <fabz@o2.pl>
2019-06-10 22:17:02 +0200

    Add Mustache to carusele.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+
+// --- MUSTACHE --- 
+
+var listItems = '';
+var templateItem = document.getElementById('template-product-item').innerHTML;
+Mustache.parse(templateItem);
+
+for(var i = 0; i < carouselData.length; i++){
+		listItems += Mustache.render(templateItem, carouselData[i]);
+	};
+
+results.insertAdjacentHTML('beforeend', listItems);
+
+//  ---- Carusel ----
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
@@ -7,18 +22,11 @@ var flkty = new Flickity( elem, {
   hash: true
 });
 
+//  ---- Progres Bar ----
+
 var progressBar = document.querySelector('.progress-bar')
 
  flkty.on('scroll', function (progress) {
      progress = Math.max(0, Math.min(1, progress));
      progressBar.style.width = progress * 100 + '%';
  });
-
- var restart = document.querySelector(".restart");
-
- restart.addEventListener('click', function () {
-     var flkty = new Flickity('.main-carousel');
-     flkty.select(0);
- })
-
-
